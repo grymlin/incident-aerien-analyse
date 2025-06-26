4afb4611ec80c29801ee5163dbc7011656163c7b
Rym Otsmane <rymotsmane@gmail.com>
2025-06-26 21:49:16 +0200

    error

--- streamlit_app.py
+++ streamlit_app.py
@@ -91,7 +91,6 @@ try:
     df = load_data()
     df = clean_data(df)
     df = add_columns(df)
-    df = df.apply(clean_operator)
 
 except Exception as e:
     st.error(f"Error while loading or cleaning data: {e}")
